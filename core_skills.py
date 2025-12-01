1f4ea077a6cc4e8395d63566266930f6ac2bf2d8
Frederico Matos <fredericomatos@Fredericos-MacBook-Pro.local>
2025-12-01 13:34:18 -0300

    Add task to check due date of Loans.

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,6 @@
 import random
-# rand_list =
+rand_list = random.sample(range(1, 20), k=10)
 
-# list_comprehension_below_10 =
+list_comprehension_below_10 = [i for i in rand_list if i < 10]
 
-# list_comprehension_below_10 =
+list_comprehension_below_10 = list(filter(lambda x: x < 10, rand_list))
